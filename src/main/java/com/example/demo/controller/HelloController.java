0535a253b570205b5a5ac3bd61c35f5a29e98058
sungtae0314.kang <sungtae0314.kang@lgepartner.com>
2023-06-05 15:30:35 +0900

    cpd test

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String home() {
         return "hello world";
     }
+
+    @GetMapping("/hello")
+    public String home2() {
+        return "hello world";
+    }
 }
